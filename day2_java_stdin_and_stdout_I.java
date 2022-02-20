ccf3c6ac7b2c1f1ff0067aae7f126e5385c45f7b
Nadia_D0221015_A <99707894+nadiadhya@users.noreply.github.com>
2022-02-20 20:04:16 +0700

    day2_java_stdin_and_stdout_I.java

---
+++ day2_java_stdin_and_stdout_I.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int x = scan.nextInt();
+        int d =scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(x);
+        System.out.println(d);
+    }
+}
